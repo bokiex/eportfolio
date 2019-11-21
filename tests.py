15e7df3d45bbd0b55dfa231d7e8a1a790e49a0ac
bokie <kohbokyan@gmail.com>
2019-11-21 12:01:01 +0800

    Add successful test case

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 timeout = 5
 
-def test_login():
+def test_unsuccessful_login():
     driver.get("http://127.0.0.1:8000/admin/")
     name = driver.find_element_by_name("username")
     password = driver.find_element_by_name("password")
@@ -18,7 +18,22 @@ def test_login():
     password.send_keys("wrongpassword")
     submit.click()
 
-def test_connection():
+def test_successful_login():
+    nameString = "bokie"
+    passString="123123"
+    driver.get("http://127.0.0.1:8000/admin/")
+    name = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("password")
+    submit = driver.find_element_by_xpath("//input[@value='Log in']")
+    name.send_keys(nameString)
+    password.send_keys(passString)
+    submit.click()
+
+def test_logout():
+    logout = driver.find_element_by_xpath("//a[text()='Log out']")
+    logout.click()
+    
+def test_project_connection():
     driver.get("http://127.0.0.1:8000/projects/")
     element_present = EC.presence_of_element_located((By.ID, "portfolioBody"))
     wait = WebDriverWait(driver,timeout).until(element_present)
@@ -26,7 +41,11 @@ def test_connection():
 
 def test_navigate_to_project():
     driver.get("http://127.0.0.1:8000/blog/")
-
+    home_button = driver.find_element_by_xpath("//a[text()='Home']")
+    home_button.click()
+    element_present = EC.presence_of_element_located((By.ID, "portfolioBody"))
+    wait = WebDriverWait(driver,timeout).until(element_present)
+    assert(wait)
 
 def test_view_project_detail():
     driver.get("http://127.0.0.1:8000/projects/")
@@ -73,7 +92,7 @@ def test_long_string_comment():
     author.send_keys("Test Name")
     body.send_keys(string)
     submit.click()
-    element_present = EC.presence_of_element_located((By.XPATH,"//div[@class='commentBody']/p[text()='TestTestTestTestTestTestTestTestTestTestTestTestTestTestTest']"))
+    element_present = EC.presence_of_element_located((By.XPATH,"//div[@class='commentBody']/p[text()='TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest']"))
     wait = WebDriverWait(driver,timeout).until(element_present)
     assert(wait)
     driver.back()
@@ -92,6 +111,20 @@ def test_long_string_name():
     body.send_keys("Test Comment")
     submit.click()
     element_present = EC.presence_of_element_located((By.XPATH,"//div[@class='commentAuthor']/p/b[text()='TestTestTestTestTest']"))
+    wait = WebDriverWait(driver,timeout).until(element_present)
+    assert(wait)
+    driver.back()
+
+def test_unique_char_name():
+    string = "MY%nameis!@#$%^&*()"
+    driver.get("http://127.0.0.1:8000/blog/1/")
+    author = driver.find_element_by_name("author")
+    body = driver.find_element_by_name("body")
+    submit = driver.find_element_by_xpath("//button[text()='Submit']")
+    author.send_keys(string)
+    body.send_keys("Test Comment")
+    submit.click()
+    element_present = EC.presence_of_element_located((By.XPATH,"//div[@class='commentAuthor']/p/b[text()='MY%nameis!@#$%^&*()']"))
     wait = WebDriverWait(driver,timeout).until(element_present)
     assert(wait)
     driver.back()
